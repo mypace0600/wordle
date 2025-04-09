70994d625a38fdd99c1f2a442c08b473faa4b879
mypace0600 <mypace0600@gmail.com>
2025-04-09 13:23:50 +0900

    LetterResult 신규 dto 생성

---
+++ src/main/java/com/wordle/quiz/dto/LetterResult.java
@@ -0,0 +1,17 @@
+package com.wordle.quiz.dto;
+
+import com.wordle.quiz.enums.LetterStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public  class LetterResult {
+    private char letter;  // 사용자가 입력한 글자
+    private LetterStatus status; // "correct" (🟩), "misplaced" (🟨), "incorrect" (⬜)
+}
