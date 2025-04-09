defaa04f89518284b1a68f8980a09252a39370c7
mypace0600 <mypace0600@gmail.com>
2025-04-09 13:24:10 +0900

    LetterResult 신규 dto 생으로 기존 로직 수정

--- src/main/java/com/wordle/quiz/dto/QuizResultResponse.java
+++ src/main/java/com/wordle/quiz/dto/QuizResultResponse.java
@@ -1,6 +1,5 @@
 package com.wordle.quiz.dto;
 
-import com.wordle.quiz.enums.LetterStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,15 +16,5 @@ public class QuizResultResponse {
     private int score;        // 획득한 점수
     private int remainingAttempts; // 남은 시도 횟수
     private List<LetterResult> feedback; // 각 글자의 Wordle 스타일 피드백
-
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class LetterResult {
-        private char letter;  // 사용자가 입력한 글자
-        private LetterStatus status; // "correct" (🟩), "misplaced" (🟨), "incorrect" (⬜)
-    }
 }
 
