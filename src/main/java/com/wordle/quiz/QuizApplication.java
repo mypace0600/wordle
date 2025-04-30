464f41cee5f3cda0c91433ed80276a8f56f2ca7d
mypace0600 <mypace0600@gmail.com>
2025-04-30 17:17:23 +0900

    redis session 관련 항목 삭제

--- src/main/java/com/wordle/quiz/QuizApplication.java
+++ src/main/java/com/wordle/quiz/QuizApplication.java
@@ -1,12 +1,9 @@
 package com.wordle.quiz;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableRedisHttpSession
 @EnableJpaAuditing
 @SpringBootApplication
 public class QuizApplication {
