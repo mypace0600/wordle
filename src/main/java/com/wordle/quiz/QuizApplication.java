fad9519622e06fe3fe756e72f264b26757a21ad0
mypace0600 <mypace0600@gmail.com>
2025-04-30 10:07:04 +0900

    배포 수정

--- src/main/java/com/wordle/quiz/QuizApplication.java
+++ src/main/java/com/wordle/quiz/QuizApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@Slf4j
 @EnableRedisHttpSession
 @EnableJpaAuditing
 @SpringBootApplication
